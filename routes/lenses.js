b660153146a073027d7fca44d69b1c8ac76ec0f9
BernieTheBear <balmanzar883@gmail.com>
2021-04-21 01:27:52 -0400

    Add files via upload

--- routes/lenses.js
+++ routes/lenses.js
@@ -1,12 +1,35 @@
+//ROUTER FOR ALL THINGS /lenses
+
+const Lens = require('../models/lens-model')
 const express = require('express')
 const router = express.Router()
 
 router.get('/new',(req,res)=>{
-    res.render('lenses/new-lens')
+    res.render('lenses/new-lens',{lens: new Lens()})
 })
 
-router.post('/',(req,res)=>{
+router.get('/:id', async (req,res)=>{
+    const lens = await Lens.findById(req.params.id)
+    if(article == null)res.redirect('/')
+    res.render('lenses/show',{lens:lens})
+})
+
+router.post('/',async (req,res)=>{
     
+    //make new lens using lens model
+    let lens = new Lens({
+        Name: req.body.Name,
+        Brand: req.body.Brand,
+        BuildDate: req.body.BuildDate,
+        Description: req.body.Description,
+    })
+    try{
+        lensID = await lens.save()
+        res.redirect(`/lenses/${lensID}`)
+    } catch(e){
+        console.log(e)
+        res.render('lenses/new-lens',{lens: lens})
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,13 +2,29 @@ const express = require('express')
 const pug = require('ejs')
 const path = require("path");
 const lensRouter = require('./routes/lenses')
+const mongoose = require('mongoose')
+const chalk = require('chalk')
+
+
 
 const app = express()
 const port = process.env.PORT || "3000";
 
+//DB
+require('dotenv').config()
+
+
+mongoose.connect('mongodb+srv://balmanzar883:test123@cluster0.xq6lw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser:true,useUnifiedTopology:true})
+const db = mongoose.connection
+db.on('error',(error)=> console.log(chalk.redBright('Not able to connect: ' + error)))
+db.once('open', ()=>{})
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine','ejs')
-app.use('/lenses',lensRouter)
+app.use(express.urlencoded({ extended: false}))
+
+
 
 app.get('/',(req,res)=>{
     const lenses = [{
@@ -26,6 +42,8 @@ app.get('/',(req,res)=>{
     res.render('lenses/index',{lenses: lenses})
     
 })
+
+app.use('/lenses',lensRouter)
 
 app.listen(port,()=>{
     console.log(`App running at port: ${port}`)
